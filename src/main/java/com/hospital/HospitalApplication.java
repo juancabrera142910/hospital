9afc6ce63607e2eb49f34adf654a9b58289a9ede
B09S112est <juan.cabreram@upb.edu.co>
2024-10-16 19:17:47 -0500

    se remueven implementaciones prueba

--- src/main/java/com/hospital/HospitalApplication.java
+++ src/main/java/com/hospital/HospitalApplication.java
@@ -1,6 +1,5 @@
 package com.hospital;
 
-import com.hospital.entitys.Persona;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,7 @@ public class HospitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
-		Persona persona  = new Persona();
-		persona.getId();
+
 	}
 
 }
